4f0770e5ed730ea4c05926e00f98a0582164af41
Jang Min <97824232+Research-Mini@users.noreply.github.com>
2024-02-15 11:34:43 +0900

    [add] make chance

--- DungeonMaker/Assets/JM/Scripts/DungeonGenerator.cs
+++ DungeonMaker/Assets/JM/Scripts/DungeonGenerator.cs
@@ -10,12 +10,32 @@ public class DungeonGenerator : MonoBehaviour
         public bool visited=false;
         public bool[] status = new bool[4];
     }
-    
-    public Vector2 size;
-    public int startPos=0;
-    public GameObject room;
-    public Vector2 offset;
+    [System.Serializable]
+    public class Rule
+    {
+        public GameObject room;
+        public Vector2Int minPosition;
+        public Vector2Int maxPosition;
 
+        public bool obligatory;
+
+        public int ProbabilityOfSpawning(int x, int y)
+        {
+            //0 - cannot spawn 1- can spawn 2- has to spawn
+
+            if(x>=minPosition.x && x<=maxPosition.x && y>=minPosition.y && y <= maxPosition.y)
+            {
+                return obligatory ? 2 : 1;
+            }
+            return 0;
+        }
+    }
+    public Vector2Int size;
+    public int startPos=0;
+    public Rule[] rooms;
+    //public GameObject room;
+    public Vector2 offset;
+    public int lastVisitedCellIndex = -1;
     List<Cell> board;
 
     // Start is called before the first frame update
@@ -32,16 +52,52 @@ public class DungeonGenerator : MonoBehaviour
 
     void GenerateDungeon()
     {
-        for(int i = 0; i < size.x; i++)
+        for (int i = 0; i < size.x; i++)
         {
-            for(int j = 0; j < size.y; j++)
+            for (int j = 0; j < size.y; j++)
             {
-                Cell currentCell =board[Mathf.FloorToInt(i+j*size.x)];
+                Cell currentCell = board[i + j * size.x];
                 if (currentCell.visited)
                 {
-                    var newRoom = Instantiate(room, new Vector3(i * offset.x, 0, -j*offset.y), Quaternion.identity, transform).GetComponent<RoomBehaviour>();
-                    newRoom.UpdateRoom(currentCell.status);
+                    int roomIndex;
+                    if (i + j * size.x == lastVisitedCellIndex)
+                    {
+                        // 마지막 방문한 셀의 위치에 room3 객체를 생성합니다.
+                        // room3가 rooms 배열에서 몇 번째 인덱스에 위치하는지 알아야 합니다.
+                        // 예를 들어, room3가 rooms 배열의 세 번째 요소라고 가정합니다.
+                        roomIndex = 2; // rooms 배열에서 room3의 인덱스
+                    }
+                    else
+                    {
+                        // 기존 로직으로 방을 선택합니다.
+                        List<int> availableRooms = new List<int>();
+                        for (int k = 0; k < rooms.Length; k++)
+                        {
+                            int probability = rooms[k].ProbabilityOfSpawning(i, j);
+                            if (probability == 2) // 필수 방
+                            {
+                                roomIndex = k;
+                                break;
+                            }
+                            else if (probability == 1) // 선택 가능한 방
+                            {
+                                availableRooms.Add(k);
+                            }
+                        }
 
+                        if (availableRooms.Count > 0)
+                        {
+                            roomIndex = availableRooms[Random.Range(0, availableRooms.Count)];
+                        }
+                        else
+                        {
+                            // 선택할 수 있는 방이 없을 경우 기본 방을 선택
+                            roomIndex = 0; // 기본 방의 인덱스
+                        }
+                    }
+
+                    var newRoom = Instantiate(rooms[roomIndex].room, new Vector3(i * offset.x, 0, -j * offset.y), Quaternion.identity, transform).GetComponent<RoomBehaviour>();
+                    newRoom.UpdateRoom(currentCell.status);
                     newRoom.name += " " + i + "-" + j;
                 }
             }
@@ -64,7 +120,7 @@ public class DungeonGenerator : MonoBehaviour
 
         int k=0;
 
-        while(k<1000)
+        while(k<36)
         {
             k++;
             
