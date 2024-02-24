34e8b31cce8ffb751025078c5e0d816679693dac
YouKwangChae <64062195+Youkwangchae@users.noreply.github.com>
2024-02-24 12:59:56 +0900

    [Add] 미니맵 기능 추가

---
+++ DungeonMaker/Assets/YGC/Scripts/Character.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YGC
+{
+    public class Character : MonoBehaviour
+    {
+        public DungeonGenerator generator;
+
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            transform.SetParent(generator.transform);
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            Debug.Log("Collsion Detect");
+
+            ChangeLayer(collision.collider.transform.parent.parent, "MinimapVisible");
+        }
+
+        public void ChangeLayer(Transform trans, string name)
+        {
+            ChangeLayersRecursively(trans, name);
+        }
+
+        public void ChangeLayersRecursively(Transform trans, string name)
+        {
+            trans.gameObject.layer = LayerMask.NameToLayer(name);
+            foreach (Transform child in trans)
+            {
+                ChangeLayersRecursively(child, name);
+            }
+        }
+    }
+}
