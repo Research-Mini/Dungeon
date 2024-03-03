5fc6e909bcd22b9ef64fc6469ae62b867b4e1795
Jang Min <97824232+Research-Mini@users.noreply.github.com>
2024-03-03 19:26:49 +0900

    [ADD] player collider modify, HP slider, Scene Change
    [ADD] player collider modify, HP slider, Scene Change

--- DungeonMaker/Assets/JM/Scripts/PlayerStats.cs
+++ DungeonMaker/Assets/JM/Scripts/PlayerStats.cs
@@ -1,13 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PlayerStats : MonoBehaviour
 {
+    public static PlayerStats instance;
+
     public int maxHealth = 100;
     public int currentHealth = 100;
     public int attackPower = 10;
     public int defensePower = 5;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        StartCoroutine(DecreaseHealthOverTime());
+        HealthUI.instance.SetMaxHealth(maxHealth); 
+    }
+
+    IEnumerator DecreaseHealthOverTime()
+    {
+        while (currentHealth > 0)
+        {
+            yield return new WaitForSeconds(1); 
+            TakeDamage(10); 
+        }
+    }
 
     public void TakeDamage(int damage)
     {
@@ -17,11 +46,40 @@ public class PlayerStats : MonoBehaviour
         {
             Die();
         }
+       
+        Debug.Log("Updating health UI.");
+        if (HealthUI.instance != null)
+        {
+            HealthUI.instance.UpdateHealth(currentHealth);
+        }
+        else
+        {
+            Debug.LogError("HealthUI.instance is null!");
+        }
     }
 
     private void Die()
     {
-        //Á×¾úÀ»¶§
         Debug.Log("Player Died");
+        
+    }
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        
+        if (HealthUI.instance != null)
+        {
+            HealthUI.instance.SetMaxHealth(maxHealth);
+            HealthUI.instance.UpdateHealth(currentHealth);
+        }
     }
 }
